326075351994c51596069adb7fdc39130743c8a6
Sergey <keerfreek@gmail.com>
2022-12-08 23:43:46 +0300

    Add Homework02

---
+++ Homework02/Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetNamber(string namberStr)
+{
+    Console.WriteLine(namberStr);
+    int namber = int.Parse(Console.ReadLine() ??"");
+    return namber;
+}
+
+int SecondNamber(int namber)
+{
+    int namberSec = (namber / 10) % 10;
+    return namberSec;
+}
+
+int namber = GetNamber($"Введите трехзначное число: ");
+
+Console.WriteLine($"Втарая цыфра трёхзначного числа {namber} = {SecondNamber(namber)}");
---
+++ Homework02/Task02/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+int GetNamber(string massege)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(massege);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine($"Введите корректное число");
+        }
+
+    }
+
+    return result;
+}
+
+string GetTreNamber(int namber)
+{
+    int namberTre = namber % 10;
+    if(namber < 1000 && namber > 99)
+        return $"Третья цифра {namberTre}";
+    if(namber > 999)
+        while(namberTre > 10)
+            namberTre = namberTre / 10;
+        return $"Третья цифра {namberTre}";
+    return $"Третьей цифры нет";
+}
+
+int namber = GetNamber("Введите число ");
+
+Console.WriteLine(GetTreNamber(namber));
