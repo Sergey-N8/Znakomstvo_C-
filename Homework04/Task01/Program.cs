4197c033d0206766233eaa1668f1c87e1223ba35
Sergey <keerfreek@gmail.com>
2022-12-15 23:15:28 +0300

    Add Homework04

---
+++ Homework04/Task01/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int GetNamber(string massege)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(massege);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine($"Введите корректное число");
+        }
+
+    }
+
+    return result;
+}
+
+
+void GetNamberPowerOff(int namberFirst, int namberSecond)
+{
+    int result = namberFirst;
+    for(int i = 1; namberSecond > i; i++)
+
+        result = result * namberFirst;
+
+    Console.WriteLine(result);
+}
+
+int namberFirst = GetNamber("Введите первое число");
+int namberSecond = GetNamber("Введите степень в которую необходимо возвести первое число");
+
+GetNamberPowerOff(namberFirst, namberSecond);
---
+++ Homework04/Task02/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+int GetNamber(string massege)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(massege);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine($"Введите корректное число");
+        }
+
+    }
+
+    return result;
+}
+
+void GetSumDigit(int namber)
+{
+    int digit = 0;
+    int namberFoResult = namber;
+    while(namber != 0)
+    {
+        digit = digit + namber % 10;
+        namber = namber / 10;
+    }
+    Console.WriteLine($"Сумма цыфр числа {namberFoResult} ровна {digit}");
+}
+
+int namber = GetNamber("Введите число");
+
+GetSumDigit(namber);
