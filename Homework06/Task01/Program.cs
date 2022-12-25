52146c3ebfe7f484d7d3810f7e105a731baf9775
Sergey <keerfreek@gmail.com>
2022-12-25 16:01:57 +0300

    Add Homework06

---
+++ Homework06/Task01/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+/*
+string RemovingSpaces (string series)
+{
+  string seriesNew = "";
+  for (int i = 0; i < series.Length; i++)
+  {
+    if (series[i] != ' ') 
+    {
+      seriesNew += series[i];
+    }
+  }
+  return seriesNew;
+} 
+*/
+
+
+
+int[] InitArray(string[] massege)
+{
+  int[] array = new int [massege.Length];
+
+  for (int i = 0; i < massege.Length; i++)
+  {
+    array[i] = Convert.ToInt32(massege[i]);
+  }
+  return array;
+}
+
+int GetPositive(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0)
+      count++;
+  }
+  return count;
+}
+
+void PrintArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]}, ");
+  }
+}
+
+Console.Write("Введите ряд чисел, разделенных запятой: ");
+string[] massege = Console.ReadLine().Split(",");
+int[] array = InitArray(massege);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Чисел больше нуля:{GetPositive(array)}");
---
+++ Homework06/Task02/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double getNumber(string message)
+{
+    double result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Введено неверное число, повторить ввод");
+        }
+    }
+    return result;
+}
+
+(double, double)  GetIntersection(double k1, double b1, double k2, double b2)
+{
+    double xСoordi = (b2 - b1)/(k1 - k2);
+
+    double yСoordi = k1 * xСoordi + b1;
+
+    return (xСoordi, yСoordi);
+}
+
+
+Console.WriteLine("Для прямой y = k1 * x + b1");
+double k1 = getNumber("Задайте k1: ");
+double b1 = getNumber("Задайте b1: ");
+Console.WriteLine("Для прямой y = k2 * x + b2");
+double k2 = getNumber("Задайте k2: ");
+double b2 = getNumber("Задайте b2: ");
+Console.WriteLine(GetIntersection(k1, b1, k2, b2));
